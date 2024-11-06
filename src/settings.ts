b91af8f9e2b9f7802c55e57864d41602e964c9c2
Rizki Gunawan <rizkigunawan300@gmail.com>
2024-11-06 14:12:39 +0800

    Update: victory message

--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,4 @@
-export const VICTORY_MESSAGE = '🎉 Congratulations! 🎉';
+export const VICTORY_MESSAGE = 'Well Done!';
 export const DEFEAT_MESSAGE = 'Better luck next time!';
 export const WORD_SIZE = 5;
 export const MAX_GUESS_COUNT = 6;
